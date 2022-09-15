f7fba9add9b29f69b5490c800753b404334759e5
Laura <moomal.abro@gmail.com>
2022-09-15 08:16:35 +0500

    send password reset email

--- src/routes/emailRouter.js
+++ src/routes/emailRouter.js
@@ -5,6 +5,7 @@ import * as email from '../controllers/email.js';
 const router = express.Router();
 
 router.get('/verify', email.verify);
+router.post('/forgot', email.forgot);
 
 
 export default router;
