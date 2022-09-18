a664d6f5ef44e01dc5f2af00ac48e19bd783588d
Laura <moomal.abro@gmail.com>
2022-09-18 12:52:43 +0500

    fix google unlink to catch errors

--- src/assets/js/account.js
+++ src/assets/js/account.js
@@ -5,8 +5,15 @@ const googleLink = document.querySelector('#google a');
 
 googleLink && googleLink.addEventListener('click', unlinkGoogle);
 
-function unlinkGoogle(e) {
+async function unlinkGoogle(e) {
 	e.preventDefault();
-	document.getElementById('revoke-google').submit();
-	setTimeout(() => location.href = "/oauth/google/revoke", 500);
+	// document.getElementById('revoke-google').submit();
+	const token = document.querySelector('[name="token"]').value;
+	const res = await fetch(`https://accounts.google.com/o/oauth2/revoke?token=${token}`, {
+			method: 'post',
+			headers: { 'content-type': 'application/x-www-form-urlencoded' }
+	});
+	const data = await res.json();
+	if ( data.error ) location.href = "/oauth/google/revoke?success=false";
+	else location.href = "/oauth/google/revoke?success=true";
 }
--- src/routes/oauthRouter.js
+++ src/routes/oauthRouter.js
@@ -10,30 +10,40 @@ dotenv.config();
 
 const router = express.Router();
 
+
+// GOOGLE
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/dashboard');
   });
+
 router.get('/google/revoke', async (req, res) => {
 	try {
+		if (req.query.success !== 'true') throw new Error();
 		const user = await User.findById(req.user._id);
 		user.googleId = null;
 		user.googleToken = null;
 		await user.save();
-		req.session.flash = { type: 'success', message: ['Google acocunt unlinked'] };
+		req.session.flash = { type: 'success', message: ['Google acocunt unlinked successfully'] };
 		res.redirect('/account');
 	} catch (err) {
-		req.session.flash = { type: 'error', message: [err.message] };
+		req.session.flash = { type: 'error', message: ["Could not unlink account"] };
 		res.redirect('/account');
 	}	
 });
 
+
+// GITHUB
+
 router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
+
 router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/dashboard');
   });
+
 router.get('/github/revoke', async (req, res) => {
 	try {
 		const octokit = new Octokit({
@@ -42,16 +52,16 @@ router.get('/github/revoke', async (req, res) => {
 				clientId: process.env.GITHUB_ID,
 				clientSecret:  process.env.GITHUB_SECRET,
 			},
-		});
-		await octokit.request(`DELETE /applications/857d1ba89cbd23d12908/grant`, { access_token: req.user.githubToken })
+		});   
+		await octokit.request(`DELETE /applications/${process.env.GITHUB_ID}/grant`, { access_token: req.user.githubToken });
 		const user = await User.findById(req.user._id);
 		user.githubId = null;
 		user.githubToken = null;
 		await user.save();
-		req.session.flash = { type: 'success', message: ['Github account unlinked'] };
+		req.session.flash = { type: 'success', message: ['Github account unlinked successfully'] };
 		res.redirect('/account');
 	} catch (err) {
-		req.session.flash = { type: 'error', message: [err.message] };
+		req.session.flash = { type: 'error', message: ["Could not unlink account."] };
 		res.redirect('/account');
 	}	
 });
