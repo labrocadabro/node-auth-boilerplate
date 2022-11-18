28578b282aa1fe1f24816c70a9bbdfbe43e79c25
Laura <moomal.abro@gmail.com>
2022-11-18 10:30:29 +0500

    reorganization

--- src/server.js
+++ src/server.js
@@ -1,46 +1,47 @@
 import express from "express";
-import path from 'path';
-import cors from 'cors';
-import passport from 'passport';
-import session from 'express-session';
-import dotenv from 'dotenv';
-import * as url from 'url';
+import path from "path";
+import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+import dotenv from "dotenv";
+import * as url from "url";
 
-import connectDB from "./config/db.js";
-import google from "./config/googleAuth.js";
-import github from "./config/githubAuth.js";
+import connectDB from "./db/config.js";
+import google from "./auth/oauth/integrations/google.js";
+import github from "./auth/oauth/integrations/github.js";
 
-import User from "./models/User.js";
+import User from "./users/User.model.js";
 
-import auth from "./middleware/auth.js";
-import flash from "./middleware/flash.js";
+import auth from "./auth/middleware.js";
+import flash from "./flash/middleware.js";
 
-import mainRouter from "./routes/mainRouter.js";
-import emailRouter from "./routes/emailRouter.js";
-import oauthRouter from "./routes/oauthRouter.js";
-
+import mainRouter from "./main/routes.js";
+import emailRouter from "./email/routes.js";
+import oauthRouter from "./auth/oauth/routes.js";
 
 // const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.set('views', './src/views');
-app.set('view engine', 'pug'); 
+app.set("views", "./src/views");
+app.set("view engine", "pug");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, "assets")));
 app.use(cors());
-app.use(session({
+app.use(
+  session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60 * 60 * 1000 * 24 } // 1 day
-}));
+    cookie: { maxAge: 60 * 60 * 1000 * 24 }, // 1 day
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(User.createStrategy());
